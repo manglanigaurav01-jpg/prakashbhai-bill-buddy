6aa6727f31d405f2d2b1fec6af3b0f440af6cc21
manglanigaurav01-jpg <manglanigaurav01@gmail.com>
2025-09-15 06:37:20 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,21 +1,57 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
+
+
 const config: CapacitorConfig = {
+
   appId: 'app.lovable.fefdfaed58db469497b3784882c36196',
+
   appName: 'prakashbhai-bill-buddy',
+
   webDir: 'dist',
+
   server: {
+
     url: 'https://fefdfaed-58db-4694-97b3-784882c36196.lovableproject.com?forceHideBadge=true',
+
     cleartext: true
+
   },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: '#3449be',
-      showSpinner: true,
-      spinnerColor: '#ffffff'
+
+  android: {
+
+    minSdkVersion: 23,
+
+    compileSdkVersion: 35,
+
+    targetSdkVersion: 34,
+
+    buildOptions: {
+
+      javaVersion: '17'
+
     }
+
+  },
+
+  plugins: {
+
+    SplashScreen: {
+
+      launchShowDuration: 2000,
+
+      backgroundColor: '#3449be',
+
+      showSpinner: true,
+
+      spinnerColor: '#ffffff'
+
+    }
+
   }
+
 };
+
+
 
 export default config;
