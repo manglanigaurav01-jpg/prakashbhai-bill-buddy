5b40fa2091c36bda8a3e9642ca86c17a3125438d
manglanigaurav01-jpg <manglanigaurav01@gmail.com>
2025-10-05 19:48:58 +0530

    login  commit

--- android/app/src/main/assets/public/assets/index-C63Ylohx.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DN4Vdz5z.js","assets/index-B6B1_6Gt.js","assets/index-Aqut0gi0.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-B6B1_6Gt.js";const o=r("Share",{web:()=>t(()=>import("./web-DN4Vdz5z.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
--- android/app/src/main/java/app/lovable/fefdfaed58db469497b3784882c36196/MainActivity.java
+++ android/app/src/main/java/app/lovable/fefdfaed58db469497b3784882c36196/MainActivity.java
@@ -1,4 +1,4 @@
-package com.manglanigaurav01.prakashbhaibillbuddy;
+package app.lovable.fefdfaed58db469497b3784882c36196;
 
 import com.getcapacitor.BridgeActivity;
 
